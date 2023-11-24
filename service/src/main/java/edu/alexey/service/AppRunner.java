39665dba9c982b72d4ac2c5124f2f8c375c17178
Alexey Kuznetsov <leoxander@yandex.ru>
2023-11-24 22:04:05 +0300

    Updated main pom, service files

--- service/src/main/java/edu/alexey/service/AppRunner.java
+++ service/src/main/java/edu/alexey/service/AppRunner.java
@@ -3,7 +3,9 @@ package edu.alexey.service;
 public class AppRunner {
 
 	public static void main(String[] args) {
+		var userService = new UserService();
 		System.out.println("Hello World");
+		System.out.println(userService.getUser(25L));
 	}
 
 }
